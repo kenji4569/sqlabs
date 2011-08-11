972935aa26f2f9e7df32c51b1ba83727cbbfeddb
kishita <yuusuuke.kishiita@gmail.com>
2011-08-11 16:36:35 +0900

    suggest_widgetの複数フォーム対応　#6

---
+++ modules/plugin_suggest_widget.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from gluon import *
+from gluon.sqlhtml import AutocompleteWidget
+
+class suggest_widget(AutocompleteWidget):
+    def callback(self):
+        if self.keyword in self.request.vars:
+            field = self.fields[0]
+            rows = self.db(field.like(self.request.vars[self.keyword]+'%'))\
+                .select(orderby=self.orderby,limitby=self.limitby,*self.fields)
+            if rows:
+                if self.is_reference:
+                    id_field = self.fields[1]
+                    raise HTTP(200,UL(*[LI(B(s[field.name]), SPAN(s[id_field.name])) \
+                                                for s in rows]).xml())
+                else:
+                    raise HTTP(200,UL(*[LI(B(s[field.name])) for s in rows]).xml())
+            else:
+                raise HTTP(200,'')
+                
+    def __call__(self,field,value,**attributes):
+        current.response.files.append(URL('static','plugin_suggest_widget/suggest.css'))
+        current.response.files.append(URL('static','plugin_suggest_widget/suggest.js'))
+        
+        default = dict(
+            _type = 'text',
+            value = (value!=None and str(value)) or '',
+            )
+        attr = SQLFORM.widgets.string._attributes(field, default, **attributes)
+        div_id = '%s_%s__div' % (field.name, self.keyword)
+        attr['_autocomplete']='off'
+        if self.is_reference:
+            key2 = '%s_%s__aux' % (field.name, self.keyword)
+            key3 = '%s_%s__auto' % (field.name, self.keyword)
+            attr['_class']='text_32'
+            if 'requires' in attr: del attr['requires']
+            attr['_name'] = key2
+            value = attr['value']
+            record = self.db(self.fields[1]==value).select(self.fields[0]).first()
+            attr['value'] = record and record[self.fields[0].name]
+            attr['_onfocus'] = ("""
+jQuery('#%(id)s').suggest(
+    '%(url)s',{name:'%(name)s', _name:'%(name)s', keyword:'%(keyword)s', 
+               resultsId:'%(div_id)s', minchars:'%(min_length)s'})""" % 
+                             dict(id=attr['_id'], url=self.url, name=field.name, div_id=div_id,
+                                  keyword=self.keyword, min_length=self.min_length))
+            return TAG[''](INPUT(**attr),INPUT(_type='hidden',_id=key3,_value=value,
+                                               _name=field.name,requires=field.requires),
+                           DIV(_id=div_id,_style='position:absolute;'))
+        else:
+            attr['_name']=field.name
+            attr['_onfocus'] = ("""
+jQuery('#%(id)s').suggest(
+    '%(url)s',{keyword:'%(keyword)s', _name:'%(name)s', 
+               resultsId:'%(div_id)s', minchars:'%(min_length)s'})""" %
+                             dict(id=attr['_id'], url=self.url, name=field.name, div_id=div_id,
+                                  keyword=self.keyword, min_length=self.min_length))
+            return TAG[''](INPUT(**attr),DIV(_id=div_id,_style='position:absolute;'))
+            
---
+++ static/plugin_suggest_widget/suggest.js
@@ -0,0 +1,250 @@
+/*
+ *	jquery.suggest 1.1 - 2007-08-06
+ *	
+ *	Uses code and techniques from following libraries:
+ *	1. http://www.dyve.net/jquery/?autocomplete
+ *	2. http://dev.jquery.com/browser/trunk/plugins/interface/iautocompleter.js	
+ *
+ *	All the new stuff written by Peter Vulgaris (www.vulgarisoip.com)	
+ *	Feel free to do whatever you want with this file
+ *
+ */
+(function($) {
+$.suggest = function(input, options) {
+  var $input = $(input).attr("autocomplete", "off");
+  
+  var timeout = false;		// hold timeout ID for suggestion results to appear
+
+  var preval = "";			// last recorded length of $input.val()
+  var cache = [];				// cache MRU list
+  var cacheSize = 0;			// size of cache in chars (bytes?)
+  
+  var $results;
+  if (options.resultsId!=null) {
+    $results = $('#'+options.resultsId);
+    $results.addClass(options.resultsClass);
+  } else {
+    $results = $(document.createElement("ul"));
+    $results.addClass(options.resultsClass).appendTo('body');
+  }
+  
+  if (options.showLoad==null) {
+    options.showLoad = function() {}
+  }
+  if (options.hideLoad==null) {
+    options.hideLoad = function() {}
+  }
+
+  resetPosition();
+  $(window)
+    .load(resetPosition)		// just in case user is changing size of page while loading
+    .resize(resetPosition);
+
+  $input.blur(function() {
+    setTimeout(function() { $results.hide() }, 200);
+  });
+
+  // I really hate browser detection, but I don't see any other way
+  if ($.browser.mozilla) {
+    $input.keyup(processKey);	// for JAP by hotcake
+  }
+  else {
+    $input.keydown(processKey);		// onkeydown repeats arrow keys in IE/Safari
+  }
+  $input.focus(suggest);
+
+  function resetPosition() {
+    // requires jquery.dimension plugin
+    var offset = $input.offset();
+    $results.css({
+        top: (offset.top + input.offsetHeight) + 'px',
+        left: offset.left + 'px'
+    });
+  }
+  function processKey(e) {
+    preval = $input.val();
+    // handling up/down/escape requires results to be visible
+    // handling enter/tab requires that AND a result to be selected
+    if ((/27$|38$|40$/.test(e.keyCode) && $results.is(':visible')) ||
+        (/^13$|^9$/.test(e.keyCode) && getCurrentResult())) {
+      if (e.preventDefault) e.preventDefault();
+      if (e.stopPropagation) e.stopPropagation();
+
+      e.cancelBubble = true;
+      e.returnValue = false;
+  
+      switch(e.keyCode) {
+        case 38: // up
+          prevResult();
+          break;
+        case 40: // down
+          nextResult();
+          break;
+        case 9:  // tab
+        case 13: // return
+              // for JAP by hotcake
+          selectCurrentResult();
+          break;
+        case 27: //	escape
+          $results.hide();
+          break;
+      }
+    } else {
+      if($('.ac_over').length==1 && e.keyCode==13) {
+          return;
+      }
+      clearTimeout(timeout);
+      timeout = setTimeout(suggest,options.delay);
+    }
+  }
+  function suggest() {
+    $input.trigger($input.attr('id') + '__unselected');
+    if (options.name != null) {
+      $('input[name='+options.name+']').val('');
+    }
+  
+    var q = $.trim($input.val());
+    if (q.length >= options.minchars) {
+      if ($input.val() != preval) {
+        preval = $input.val();
+        cached = checkCache(q);
+        if (cached) {
+          displayItems(cached['html']);
+        } else {
+          var query = {}
+          query[options.keyword] = q;
+
+          jQuery.ajax({type: "POST", url: options.source, data: query, 
+            'beforeSend':function(xhr){options.showLoad();},// web2py specific
+            'complete':function(xhr,text){options.hideLoad();},// web2py specific
+            success: function(html) {
+              $results.hide();
+              displayItems(html);
+              addToCache(q, html, html.length);
+            }});
+        }
+      }
+    } else {
+      $results.hide();
+    }
+  }
+  function checkCache(q) {
+    for (var i = 0; i < cache.length; i++)
+      if (cache[i]['q'] == q) {
+        cache.unshift(cache.splice(i, 1)[0]);
+        return cache[0];
+      }
+    return false;
+  }
+  function addToCache(q, html, size) {
+    while (cache.length && (cacheSize + size > options.maxCacheSize)) {
+      var cached = cache.pop();
+      cacheSize -= cached['size'];
+    }
+    cache.push({
+      q: q,
+      size: size,
+      html: html });
+    cacheSize += size;
+  }
+  function displayItems(html) {
+    if(html=='') {
+      $input.addClass(options.noMatchClass);
+      $input.removeClass(options.matchClass);
+      $input.removeClass(options.selectedClass); 
+    } else {
+      $input.addClass(options.matchClass);
+      $input.removeClass(options.noMatchClass);
+      $input.removeClass(options.selectedClass); 
+    }
+    if (!html) return;
+    if (!html.length) {
+      $results.hide();
+      return;
+    }
+    $results.html(html).show();
+    
+    $results
+      .find('li')
+      .mouseover(function() {
+        $results.find('li').removeClass(options.selectClass);
+        $(this).addClass(options.selectClass);
+      })
+      .click(function(e) {
+        e.preventDefault(); 
+        e.stopPropagation();
+        selectCurrentResult();
+      });
+  }
+  function getCurrentResult() {
+    var $currentResult = $results.find('li.' + options.selectClass);
+    
+    if (!$currentResult.length)
+      $currentResult = false;
+    return $currentResult;
+  }
+  function selectCurrentResult() {
+    $currentResult = getCurrentResult();
+    if ($currentResult) {
+      
+      $input.addClass(options.selectedClass);  
+      $input.removeClass(options.matchClass);
+      $input.removeClass(options.noMatchClass);  
+      
+      var val = $currentResult.children('b').text();
+      $input.val(val);
+      if (options.name != null) {
+        val = $currentResult.children('span').text();
+        $('input[name='+options.name+']').val(val);
+      }
+      $input.trigger($input.attr('id') + '__selected', [val]);
+      
+      $results.hide();
+      if (options.onSelect) options.onSelect($currentResult.text());
+    }
+  }
+  function nextResult() {
+    $currentResult = getCurrentResult();
+    if ($currentResult)
+      $currentResult
+        .removeClass(options.selectClass)
+        .next()
+          .addClass(options.selectClass);
+    else
+      $results.find('li').slice(0,1).addClass(options.selectClass);
+  }
+  function prevResult() {
+    $currentResult = getCurrentResult();
+    if ($currentResult)
+      $currentResult
+        .removeClass(options.selectClass)
+        .prev()
+            .addClass(options.selectClass);
+    else
+      $results.find('li').slice(-1).addClass(options.selectClass);
+  }
+}
+$.fn.suggest = function(source, options) {
+  if (!source) return;
+  options = options || {};
+  options.source = source;
+  options.delay = options.delay || 100;
+  options.resultsClass = options.resultsClass || 'ac_results';
+  options.selectClass = options.selectClass || 'ac_over';
+  options.selectedClass = options.selectedClass || 'ac_selected';
+  options.matchClass = options.matchClass || 'ac_match';
+  options.noMatchClass = options.noMatchClass || 'ac_no_match';
+  options.minchars = options.minchars || 2;
+  options.delimiter = options.delimiter || '\n';
+  options.onSelect = options.onSelect || false;
+  options.maxCacheSize = options.maxCacheSize || 65536;
+  options.keyword = options.keyword || 'q';
+  options.name = options.name || null;
+  options.resultsId = options.resultsId || null;
+
+  this.each(function() {
+      new $.suggest(this, options);
+  });
+
+  return this;
+};})(jQuery);
