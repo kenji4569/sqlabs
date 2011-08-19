c605ae876d28259bb15dfc5afd42097f5c77d3a1
Hosoda Kenji <hosoda@s-cubism.jp>
2011-08-19 13:13:40 +0900

    uploadify widget 実装完了 closed #30

--- controllers/plugin_uploadify_widget.py
+++ controllers/plugin_uploadify_widget.py
@@ -1,23 +1,41 @@
 # -*- coding: utf-8 -*-
-from plugin_uploadify_widget import uploadify_widget
+from plugin_uploadify_widget import (
+    uploadify_widget, IS_UPLOADIFY_IMAGE, IS_UPLOADIFY_FILENAME, IS_UPLOADIFY_LENGTH
+)
 
 db.define_table('uploadify', 
-    Field('image', 'upload'))
+    Field('image', 'upload', autodelete=True),
+    Field('text', 'upload', autodelete=True))
     
 db.uploadify.image.widget = uploadify_widget
+db.uploadify.image.requires = [IS_UPLOADIFY_IMAGE(), IS_UPLOADIFY_LENGTH(10240)]
+
+db.uploadify.text.widget = uploadify_widget
+db.uploadify.text.requires = IS_EMPTY_OR([IS_UPLOADIFY_FILENAME(extension='txt'), 
+                                          IS_UPLOADIFY_LENGTH(1024)])
+    
 if db(db.uploadify.id>0).count() > 5:
     last = db(db.uploadify.id>0).select(orderby=~db.uploadify.id).first()
     db(db.uploadify.id<=last.id-5).delete()
 
 def index():
-    form = SQLFORM(db.uploadify)
+    form = SQLFORM(db.uploadify, upload=URL('download'))
     if form.accepts(request.vars, session):
         session.flash = 'submitted %s' % form.vars
         redirect(URL('index'))
     
     records = db(db.uploadify.id>0).select(orderby=~db.uploadify.id)
-    print records
-    return dict(form=form, records=SQLTABLE(records, upload=URL('download')))
+    records = SQLTABLE(records, upload=URL('download'), linkto=lambda f, t, r: URL('edit', args=f))
+    return dict(form=form, records=records)
+    
+def edit():
+    record = db(db.uploadify.id==request.args(0)).select().first() or redirect('index')
+    form = SQLFORM(db.uploadify, record, upload=URL('download'))
+    if form.accepts(request.vars, session):
+        session.flash = 'edit %s' % form.vars
+        redirect(URL('edit', args=request.args))
+    
+    return dict(back=A('back', _href=URL('index')), form=form)
     
 def download():
     return response.download(request,db)
--- modules/plugin_uploadify_widget.py
+++ modules/plugin_uploadify_widget.py
@@ -1,5 +1,44 @@
 # -*- coding: utf-8 -*-
 from gluon import *
+from gluon.validators import translate
+from gluon.sqlhtml import UploadWidget
+from gluon.storage import Storage
+
+BUTTON_TEXT = 'SELECT FILES'
+
+class IS_UPLOADIFY_IMAGE(IS_IMAGE):
+    def _call(self, value): 
+        return IS_IMAGE.__call__(self, value)
+    def __call__(self, value): 
+        if not value:
+            return (value, translate(self.error_message))
+        return (value, None)
+        
+class IS_UPLOADIFY_FILENAME(IS_UPLOAD_FILENAME):
+    def __init__(self, filename=None, extension=None, lastdot=True, case=1,
+                    error_message='enter valid filename'):
+        import re
+        if extension and re.match('^[0-9a-zA-Z\.]+$', extension):
+            self._extension = extension
+        else:
+            self._extension = None
+        IS_UPLOAD_FILENAME.__init__(self, filename, extension, lastdot, case, error_message)
+        
+    def _call(self, value): 
+        return IS_UPLOAD_FILENAME.__call__(self, value)
+    def __call__(self, value): 
+        if not value:
+            return (value, translate(self.error_message))
+        return (value, None)
+        
+class IS_UPLOADIFY_LENGTH(IS_LENGTH):
+    def _call(self, value): 
+        return IS_LENGTH.__call__(self, value)
+    def __call__(self, value): 
+        if not value:
+            return (value, translate(self.error_message))
+        return (value, None)
+
 
 def _set_files():
     for _url in ( URL('static', 'plugin_uploadify_widget/uploadify.css'),
@@ -19,40 +58,120 @@ def uploadify_widget(field, value, download_url=None, **attributes):
             
     _file_id = '__uploadify__%s_%s' % (field._tablename, field.name)
     file_input_el = INPUT(_id = _file_id, _name = '__uploadify__%s' % field.name, _type='file')
-            
-    script = SCRIPT("""
-jQuery(document).ready(function() {
-var el = jQuery('#%(id)s');
-var file_el = jQuery('#%(file_id)s');
-var form = jQuery(el.get(0).form);
-file_el.uploadify({
-'uploader'  : '%(uploader)s',
-'script'    : '%(script)s',
-'cancelImg' : '%(cancel_img)s',
-'auto'      : true,
-'scriptData': {'formkey': form.find('input[name=_formkey]').val(),
-               'formname': form.find('input[name=_formname]').val()},
-'onComplete'  : function(event, ID, fileObj, response, data) {
-      el.val(response);
-    }
-});
-
-});""" % dict(id=_id, file_id=_file_id, 
-              uploader=URL('static', 'plugin_uploadify_widget/uploadify.swf'),
-              script=URL(args=current.request.args, vars=current.request.vars),
-              cancel_img=URL('static', 'plugin_uploadify_widget/cancel.png'),
-         ))
-          
-    print field.uploadfolder
-    if 'Filedata' in current.request.post_vars:
-        print '===' # TODO verify
-        print current.session
-        print current.request.post_vars
+        
+    ajaxed = 'Filedata' in current.request.post_vars and current.request.post_vars.name == field.name
+        
+    fileext = '*.*'
+    size_limit = 1000000000000
+    not_empty_message = None
+    
+    requires = field.requires
+    if isinstance(requires, IS_EMPTY_OR):
+        requires = requires.other
+        not_empty_message = ''
+        
+    if type(requires) not in (list, tuple):
+        requires = [requires]
+    for i, r in enumerate(requires):
+        if i == 0 and not_empty_message is None and not value:
+            not_empty_message = r.error_message
+        
+        if isinstance(r, IS_UPLOADIFY_IMAGE):
+            fileext = ';'.join(['*.%s' % ext if ext!='jpeg' else '*.jpeg;*.jpg' for ext in r.extensions])
+        elif isinstance(r, IS_UPLOADIFY_FILENAME):
+            if r._extension:
+                fileext = '*.%s' % r._extension
+        elif isinstance(r, IS_UPLOADIFY_LENGTH):
+            size_limit = r.maxsize
+        
+        if isinstance(r, (IS_UPLOADIFY_IMAGE, IS_UPLOADIFY_FILENAME, IS_UPLOADIFY_LENGTH)) and ajaxed:
+            f = current.request.post_vars.Filedata
+            (value, error) = r._call(f)
+            if error:
+                raise HTTP(507, error)
+    
+    if ajaxed:
         f = current.request.post_vars.Filedata
         newfilename = field.store(f.file, f.filename)
         raise HTTP(200, newfilename)
         
+    script = SCRIPT("""
+var uploadify_uploading_%(id)s = [];
+var uploadify_uploaded_%(id)s = [];
+jQuery(document).ready(function() {
+var el = jQuery('#%(id)s');
+var file_el = jQuery('#%(file_id)s');
+var form_el = jQuery(el.get(0).form);
+form_el.submit(function() {
+    var not_empty_message = "%(not_empty_message)s";
+    if (not_empty_message!="" && uploadify_uploading_%(id)s.indexOf(file_el.attr('id'))==-1) {
+        el.parent().prepend(jQuery("<div class='error'>"+not_empty_message+"</div>"));
+        return false;
+    }
+    
+    file_el.uploadifyUpload();
+    if (uploadify_uploading_%(id)s.length!=0) { return false; }
+    else { return true; }
+});
+file_el.uploadify({
+'buttonText': '%(button_text)s',
+'uploader'  : '%(uploader)s',
+'script'    : '%(script)s',
+'cancelImg' : '%(cancel_img)s',
+'auto'      : false,
+'onSelect'    : function(event,ID,fileObj) {
+    uploadify_uploading_%(id)s.push(file_el.attr('id'));
+},
+'onComplete': function(event, ID, fileObj, response, data) {
+    el.val(response);
+    uploadify_uploaded_%(id)s.push(file_el.attr('id'));
+    if (uploadify_uploading_%(id)s.length == uploadify_uploaded_%(id)s.length) {
+        setTimeout(function(){form_el.get(0).submit();}, 200);
+    }
+},
+'fileExt'   : '%(fileext)s',
+'fileDesc'    : '%(fileext)s',
+'sizeLimit' : %(size_limit)i,
+'scriptData': {'name': el.attr('name')}
+});
+
+});""" % dict(id=_id, file_id=_file_id, 
+              button_text=BUTTON_TEXT,
+              uploader=URL('static', 'plugin_uploadify_widget/uploadify.swf'),
+              script=URL(args=current.request.args, vars=current.request.vars),
+              cancel_img=URL('static', 'plugin_uploadify_widget/cancel.png'),
+              fileext=fileext,
+              size_limit=size_limit,
+              not_empty_message=not_empty_message or '',
+         ))
+          
     if '_formkey' in current.request.vars:
-        field.type = 'string'
+        filename = current.request.vars[field.name]
+        if filename:
+            current.request.vars[field.name] = Storage(file=None, filename=filename) # TODO file
+            field.store = lambda source_file, original_filename: original_filename
+        
+    inp = SPAN(input_el, file_input_el)
+    
+    if download_url and value:
+        url = download_url + '/' + value
+        (br, image) = ('', '')
+        if UploadWidget.is_image(value):
+            br = BR()
+            image = IMG(_src = url, _width = UploadWidget.DEFAULT_WIDTH)
+
+        requires = field.requires
+        if requires == [] or isinstance(requires, IS_EMPTY_OR):
+            inp = DIV(inp, '[',
+                      A(UploadWidget.GENERIC_DESCRIPTION, _href = url),
+                      '|',
+                      INPUT(_type='checkbox',
+                            _name=field.name + UploadWidget.ID_DELETE_SUFFIX),
+                      UploadWidget.DELETE_FILE,
+                      ']', br, image)
+        else:
+            inp = DIV(inp, '[',
+                      A(UploadWidget.GENERIC_DESCRIPTION, _href = url),
+                      ']', br, image)
             
-    return DIV(script, input_el, file_input_el, **attributes)
+    return DIV(script, inp, **attributes)
