d6c6c0c3dac88a1f5da7170233d6034405d49c56
Hosoda Kenji <hosoda@s-cubism.jp>
2011-08-29 12:10:22 +0900

    fix some minor issues closed #36

--- models/db.py
+++ models/db.py
@@ -7,7 +7,7 @@ response.generic_patterns = ['*'] if request.is_local else []
 info_products = dict(
     web2py_plugins= dict(
         label='web2py-plugins',
-        description=XML(T("""A collection of plugins of %s, a opensource Python web framework.
+        description=XML(T("""A collection of plugins of %s, an opensource Python web framework.
 Here we love to share useful code parts produced by our development with the framework.
 The code parts are organized in %s, and easily available.""") % (
         A('Web2py', _href='http://www.web2py.com').xml(), 
