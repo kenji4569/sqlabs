6c8930e0178926054e8749ef66c6cc8091635585
Hosoda Kenji <hosoda@s-cubism.jp>
2011-09-05 18:26:33 +0900

    refactoring elrte editor

--- controllers/plugin_elrte_widget.py
+++ controllers/plugin_elrte_widget.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-from plugin_elrte_widget import ElrteWidget
+from plugin_elrte_widget import ElrteWidget, Dialog
 from plugin_uploadify_widget import (
     uploadify_widget, IS_UPLOADIFY_IMAGE, IS_UPLOADIFY_LENGTH
 )
-import gluon.contrib.simplejson as json
 
 # use disk_db for image storing
 disk_db = db
@@ -29,28 +28,14 @@ def index():
     except:
         lang = 'en'
 
-    # set image chooser window
-    import uuid
-    mmodal_id = str(uuid.uuid4())
-    def get_mmodal(content, title='Select', close="close", width=70, height=70):
-        # original by web2py's welcome application
-        return ("""<div id="%(id)s" class="plugin_mmodal" style="display:none"><div style="position:absolute;top:0%%;left:0%%;width:100%%;height:100%%;background-color:black;z-index:1001;-moz-opacity:0.8;opacity:.80;opacity:0.8;"></div>
-                   <div class="dialog" style="position:absolute;top:%(top)s%%;left:%(left)s%%;width:%(width)s%%;height:%(height)s%%;padding:16px;border:2px solid black;background-color:white;opacity:1.0;z-index:1002;overflow:auto;">
-                   <span style="font-weight:bold">%(title)s</span><span style="float:right">[<a href="#" onclick="jQuery(\'#%(id)s\').hide();return false;">%(close)s</a>]</span><hr/><div id="c%(id)s">%(content)s</div></div></div>""" % 
-                dict(title=title,content=content,close=close,id=mmodal_id,left=(100-width)/2,top=(100-height)/2,width=width,height=height))
-    
-    fm_open = """function(callback){
-var el = jQuery("#%s");
-if (el.length == 0) {el = jQuery(%s); jQuery(document.body).append(el);}
-var maxZ = Math.max.apply(null,jQuery.map(jQuery('body > *'), function(e,n){
-           return parseInt(jQuery('.ui-dialog').css('zIndex'))||1 ;
-           }));
-el.children('.dialog').css('zIndex', maxZ + 10); 
-el.show();
+    image_chooser = Dialog(title='Select', 
+                           content=LOAD('plugin_elrte_widget', 'upload_or_choose', ajax=True))
+    file_chooser = Dialog(title='Select', 
+                          content=LOAD('plugin_elrte_widget', 'upload_or_choose', ajax=True))
+    fm_open = """function(callback, kind){
+if (kind == 'elfinder') {%s();} else {%s();}
 jQuery.data(document.body, 'elrte_callback', callback)
-}""" % (mmodal_id, json.dumps(get_mmodal(
-            content=LOAD('plugin_elrte_widget', 'upload_or_choose', ajax=True).xml().replace('<!--', '').replace('//-->', ''),
-    )))
+}""" % (file_chooser.show_js_handler(), image_chooser.show_js_handler())
 
     ################################ The core ######################################
     # Inject the elrte widget
@@ -75,7 +60,7 @@ def upload_or_choose():
     records = disk_db(image_table.id>0).select(orderby=~image_table.id, limitby=(0,3))
     _get_src = lambda r: URL(request.controller, 'download', r.image)
     records = DIV([IMG(_src=_get_src(r), 
-                       _onclick="jQuery.data(document.body, 'elrte_callback')('%s');jQuery('.plugin_mmodal').hide();" % _get_src(r),
+                       _onclick="jQuery.data(document.body, 'elrte_callback')('%s');jQuery('.dialog').hide();" % _get_src(r),
                        _style='max-width:50px;max-height:50px;margin:5px;cursor:pointer;') 
                     for r in records])
     return BEAUTIFY(dict(form=form, info=info, records=records))
--- modules/plugin_elrte_widget.py
+++ modules/plugin_elrte_widget.py
@@ -41,7 +41,7 @@ opts.panels.default_2 = ['formatblock', 'fontsize', 'fontname'];
 opts.panels.default_3 = ['bold', 'italic', 'underline', 'strikethrough', 'subscript', 'superscript'];
 opts.panels.default_4 = ['forecolor', 'hilitecolor', 'justifyleft', 'justifyright',
                   'justifycenter', 'justifyfull', 'outdent', 'indent'];
-opts.panels.default_5 = ['link', 'unlink', 'image', 'insertorderedlist', 'insertunorderedlist',
+opts.panels.default_5 = ['link', 'unlink', 'image', 'elfinder','insertorderedlist', 'insertunorderedlist',
                   'horizontalrule', 'blockquote', 'div', 'stopfloat', 'css', 'nbsp'];
 opts.toolbars = {'default': ['default_1', 'default_2', 'default_3', 'default_4', 'default_5', 'tables']};
 
@@ -59,3 +59,41 @@ el.elrte({cssClass: 'el-rte', lang: '%(lang)s', toolbar: '%(toolbar)s', fmOpen :
         
         return SPAN(script, TEXTAREA((value!=None and str(value)) or '', **attr), **attributes)
        
+class Dialog(DIV):
+        
+    def __init__(self, title, content, close='close', width=70, height=70, **attributes):
+        DIV.__init__(self, **attributes)
+        self.title, self.content, self.close, self.width, self.height = (
+            title, content, close, width, height)
+        self.attributes['_class'] = 'dialog'
+        import uuid
+        self.attributes['_id'] = self.attributes.get('_id') or str(uuid.uuid4())
+        self.attributes['_style'] = self.attributes.get('_style', 'display:none;z-index:1001;position:absolute;top:0%;left:0%;width:100%;height:100%;')
+        
+    def show_js_handler(self):
+        import gluon.contrib.simplejson as json
+        return """(function(){
+var el = jQuery("#%(id)s");
+if (el.length == 0) {el = jQuery(%(xml)s); jQuery(document.body).append(el);}
+el.css('zIndex', (parseInt(el.css('zIndex')) || 1000) + 10);
+var top  = jQuery(document).scrollTop();
+el.css('top', top);
+el.show();})"""  % dict(id=self.attributes['_id'], 
+                       xml=json.dumps(self.xml().replace('<!--', '').replace('//-->', '')))
+        
+    def xml(self): 
+        self.components += (
+            DIV(_style='background-color:black;-moz-opacity:0.5;opacity:.50;opacity:0.5;width:100%;height:100%;',
+                _class='dialog-back'),
+            DIV(SPAN(self.title, _style='font-weight:bold'),
+                SPAN('[', A(self.close, _href='#', 
+                           _onclick="jQuery('#%s').hide();return false;" % self.attributes['_id']), ']', 
+                     _style='float:right',),
+                HR(),
+                DIV(self.content, _id='c%s' % self.attributes['_id']),
+                _style=('position:absolute;top:%(top)s%%;left:%(left)s%%;width:%(width)s%%;height:%(height)s%%;padding:16px;border:2px solid black;background-color:white;opacity:1.0;z-index:1002;overflow:auto;' %
+                    dict(left=(100-self.width)/2,top=(100-self.height)/2,width=self.width,height=self.height)),
+                _class='dialog-front',
+            ),
+        )
+        return DIV.xml(self)
