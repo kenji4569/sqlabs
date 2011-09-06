8dd276fe574a643f0ff06a4b06ebbe4c3d55be17
kishita <yuusuuke.kishiita@gmail.com>
2011-09-06 22:28:34 +0900

    エラー処理、IDによるクエリ、関数追加。 #41

--- controllers/plugin_mptt.py
+++ controllers/plugin_mptt.py
@@ -7,7 +7,32 @@ def index():
     mptt = MPTT(db)
     mptt.define_tables()
     db.tree.insert(title='Food', left=1, right=2)
-    mptt.test()
     return dict()
     
+def test():
+    mptt = MPTT(db)
+    mptt.define_tables()
+    db.tree.insert(title='Food', left=1, right=2)
+    
+    mptt.add_node('Fruit', 'Food')
+    mptt.add_node('Meat', 'Food')
+    mptt.add_node('Red', 'Food')
+    mptt.add_node('Rabbit', 'Meat')
+    mptt.add_node('Pig', 'Meat')
+    print db().select(db.tree.ALL)
+        
+    #print self.get_ancestors('Pig', db.tree.title)
+    #print self.get_descendants('Fruit', db.tree.title)
+    #print self.get_descendant_count('Food')
+    #print mptt.get_leafnodes(db.tree.title)
+    #print mptt.get_root()
+    #print self.is_child('Food')
+    #print self.is_child('Rabbit')
+    #print self.is_root_node('Rabbit')
+    #print self.is_root_node('Food')
+    #print self.is_leaf_node('Rabbit')
+    #print self.is_leaf_node('Meat')
+    #print self.remove_node('Food')
 
+    output = db().select(db.tree.ALL)
+    return dict(output=output)
--- modules/plugin_mptt.py
+++ modules/plugin_mptt.py
@@ -29,32 +29,55 @@ class MPTT(object):
          
     def add_node(self, title, parent_title):
         db = self.db
-        #table = table
-        parent = db(db.tree.title == parent_title).select()[0]
+        
+        parent = db(db.tree.title == parent_title).select().first()
+        if not parent:
+            raise ValueError
+        
         db(db.tree.right >= parent.right).update(right=db.tree.right + 2)
         db(db.tree.left >= parent.right).update(left=db.tree.left + 2)
         db.tree.insert(title=title, left=parent.right, right=parent.right + 1)
     
-    def get_ancestors(self, title, *fields):
+    def get_ancestors(self, id, *fields):
         db = self.db
         """print ancestors('Red', db.tree.title)"""
-        node = db(db.tree.title == title).select()[0]
+        node = db(db.tree.id == id).select().first()
+        if not node:
+            raise ValueError
         return db(db.tree.left < node.left)(db.tree.right > node.right).select(orderby=db.tree.left, *fields)
     
     def get_descendants(self, title, *fields):
         db = self.db
         """print descendants('Fruit', db.tree.title)"""
-        node = db(db.tree.title == title).select()[0]
+        node = db(db.tree.title == title).select().first()
+        if not node:
+            raise ValueError
         return db(db.tree.left > node.left)(db.tree.right < node.right).select(orderby=db.tree.left, *fields)
     
     def get_descendant_count(self, title):
         db = self.db
-        node = db(db.tree.title == title).select()[0]
-        return (node.right - node.left - 1) / 2   
+        node = db(db.tree.title == title).select().first()
+        if not node:
+            raise ValueError
+        return (node.right - node.left - 1) / 2,   
     
-    def get_leafnodes(self):
+    def get_leafnodes(self, *fields):
         db = self.db
-        return db(db.tree.right == db.tree.left+1).select(orderby=db.tree.left)
+        return db(db.tree.right == db.tree.left+1).select(orderby=db.tree.left, *fields)
+    
+    def get_next_sibling(self, title, *fields):
+        db = self.db
+        node = db(db.tree.title == title).select().first()
+        if not node:
+            raise ValueError
+        return db(db.tree.left == node.right + 1).select(orderby=db.tree.title, *fields)
+    
+    def get_previous_sibling(self, title, *fields):
+        db = self.db
+        node = db(db.tree.title == title).select().first()
+        if not node:
+            raise ValueError
+        return db(db.tree.right == node.left - 1).select(orderby=db.tree.title, *fields)
     
     def get_root(self):
         db = self.db
@@ -85,28 +108,3 @@ class MPTT(object):
         db = self.db
         tmp = db(db.tree.title == title).select().first()
             
-    def test(self):
-        db = self.db
-        print db().select(db.tree.ALL)
-        self.add_node('Fruit', 'Food')
-        print db().select(db.tree.ALL)
-        self.add_node('Meat', 'Food')
-        print db().select(db.tree.ALL)
-        self.add_node('Red', 'Food')
-        print db().select(db.tree.ALL)
-        self.add_node('Rabbit', 'Meat')
-        print db().select(db.tree.ALL)
-
-        print self.get_ancestors('Red', db.tree.title)
-        print self.get_descendants('Food', db.tree.title)
-        print self.get_descendant_count('Meat')
-        print self.get_leafnodes()
-        print self.get_root()
-        print self.is_child('Food')
-        print self.is_child('Rabbit')
-        print self.is_root_node('Rabbit')
-        print self.is_root_node('Food')
-        print self.is_leaf_node('Rabbit')
-        print self.is_leaf_node('Meat')
-        
-        print self.remove_node('Food')
