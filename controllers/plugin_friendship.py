6f54c00a4aa199155da9e8a91798ba1c96c4aaaf
Hosoda Kenji <hosoda@s-cubism.jp>
2011-09-15 19:28:04 +0900

    refine friendship plugin #51

--- controllers/plugin_friendship.py
+++ controllers/plugin_friendship.py
@@ -36,17 +36,21 @@ for i in range(1, num_users+1):
         user_ids[i] = user.id
 
 import datetime
-db(db['plugin_friendship_friend'].created_at<
-    request.now-datetime.timedelta(minutes=10)).delete()
+deleted = db(db['plugin_friendship_friend'].created_at<
+            request.now-datetime.timedelta(minutes=10)).delete()
+if deleted:
+    friendship.refresh_all_mutuals()
 
 def index():
     user_no = int(request.args(0) or 1)
     user_id = user_ids[user_no]
     
-    if 'request_friend' in request.vars:
-        friendship.request_friend(user_id, request.vars.request_friend)
-        session.flash = 'requesting friend'
-        redirect(URL('index', args=user_no))
+    for action in ('add_friend', 'confirm_friend', 'ignore_friend', 'remove_friend'):
+        if action in request.vars:
+            getattr(friendship, action)(user_id, request.vars[action])
+            session.flash = 'done: %s' % action
+            redirect(URL('index', args=user_no))
+            break
     
     user_chooser = []
     for i in range(1, num_users+1):
@@ -56,40 +60,43 @@ def index():
             user_chooser.append(A('user%s' % i, _href=URL('index', args=i)))
         user_chooser.append(SPAN(' '))
         
-    # friendship.request_friend(user_ids[1], user_ids[2])
-    # friendship.accept_friend(user_ids[2], user_ids[1])
-    
     table_user = auth.settings.table_user
     table_friend = friendship.settings.table_friend
-    table_friend_status = friendship.settings.table_friend_status
     
-    users = db(table_user.id.belongs(set(user_ids.values())-set([user_id]))).select(
-               table_user.ALL, table_friend_status.status,
-               left=table_friend_status.on(
-                        (table_friend_status.friend==table_user.id) & 
-                        (table_friend_status.user==user_id))
-               )
-               
     friends = []
-    for user in users:
-        status = user[friendship.settings.table_friend_status_name].status
-        if status == friendship.settings.status_requesting:
+    records = db(table_user.id.belongs(set(user_ids.values())-set([user_id]))).select(
+                       table_user.ALL, table_friend.ALL, 
+                       left=table_friend.on(
+                                (table_friend.friend==table_user.id) & 
+                                (table_friend.user==user_id)))
+    for record in records:
+        status = record[table_friend].status
+        if status is None:
+            option = A('Add friend', 
+                       _href=URL('index', args=user_no, vars={'add_friend': record[table_user].id}))
+        elif status == friendship.settings.status_requesting:
             option = 'requesting'
-        elif status is None:
-            option = 'add friend'
+        elif status == friendship.settings.status_denied:
+            option = 'denied'
         else:
-            option = ''
-        friends.append(DIV(user.auth_user.email[:5], option))
+            option = SPAN('mutual friends -> %s' % record[table_friend].mutual, ' ',
+                          A('Remove friend',
+                            _href=URL('index', args=user_no, vars={'remove_friend': record[table_user].id})))
+        friends.append(DIV(record.auth_user.email[:5], ': ', option))
     
-    # friends = friendship.friends(user_id).select(
-                    # table_user.ALL, left=table_user.on(table_user.id==table_friend.friend))
+    friend_requests = []
+    records = friendship.friend_requets(user_id).select(
+                        table_user.ALL,
+                        left=table_user.on(table_user.id==table_friend.user))
+    for record in records:
+        friend_requests.append(DIV(record.email[:5], ': ',
+           A('Confirm', _href=URL('index', args=user_no, vars={'confirm_friend': record.id})), ' ',
+           A('Not now', _href=URL('index', args=user_no, vars={'ignore_friend': record.id}))))
     
     return dict(choose_user=DIV(*user_chooser),
                 friends=friends,
-                # request_friends=[
-                    # A('user%s' % no, _href=URL('index', args=user_no, vars={'request_friend': user_ids[no]})) 
-                                # for no in not_yet_friend_nos],
-                # users=users,
+                friend_requests=friend_requests,
+                tests=A('unit test', _href=URL('test')),
                 )
     
 class TestFriendship(unittest.TestCase):
@@ -97,16 +104,16 @@ class TestFriendship(unittest.TestCase):
     def setUp(self):
         friendship.settings.table_friend.truncate()
 
-    def test_request_friend(self):
-        friendship.request_friend(user_ids[1], user_ids[2])
+    def test_add_friend(self):
+        friendship.add_friend(user_ids[1], user_ids[2])
         self.assertEqual(friendship.friend_requets(user_ids[1]).count(), 0)
         self.assertEqual(friendship.friend_requets(user_ids[2]).count(), 1)
         self.assertEqual([r.user for r in friendship.friend_requets(user_ids[2]).select()], 
                          [user_ids[1]])
                          
-    def test_accept_friend(self):
-        friendship.request_friend(user_ids[1], user_ids[2])
-        friendship.accept_friend(user_ids[2], user_ids[1])
+    def test_confirm_friend(self):
+        friendship.add_friend(user_ids[1], user_ids[2])
+        friendship.confirm_friend(user_ids[2], user_ids[1])
         self.assertEqual(friendship.friend_requets(user_ids[1]).count(), 0)
         self.assertEqual(friendship.friend_requets(user_ids[2]).count(), 0)
         
@@ -119,8 +126,8 @@ class TestFriendship(unittest.TestCase):
         self.assertEqual([r.mutual for r in friends], [0])
         
     def test_remove_friend(self):
-        friendship.request_friend(user_ids[1], user_ids[2])
-        friendship.accept_friend(user_ids[2], user_ids[1])
+        friendship.add_friend(user_ids[1], user_ids[2])
+        friendship.confirm_friend(user_ids[2], user_ids[1])
         friendship.remove_friend(user_ids[1], user_ids[2])
         
         friends = friendship.friends(user_ids[1]).select()
@@ -130,23 +137,23 @@ class TestFriendship(unittest.TestCase):
         
     
     def test_mutual(self):
-        friendship.request_friend(user_ids[1], user_ids[2])
-        friendship.accept_friend(user_ids[2], user_ids[1])
+        friendship.add_friend(user_ids[1], user_ids[2])
+        friendship.confirm_friend(user_ids[2], user_ids[1])
         
-        friendship.request_friend(user_ids[2], user_ids[3])
-        friendship.accept_friend(user_ids[3], user_ids[2])
+        friendship.add_friend(user_ids[2], user_ids[3])
+        friendship.confirm_friend(user_ids[3], user_ids[2])
         
-        friendship.request_friend(user_ids[3], user_ids[1])
-        friendship.accept_friend(user_ids[1], user_ids[3])
+        friendship.add_friend(user_ids[3], user_ids[1])
+        friendship.confirm_friend(user_ids[1], user_ids[3])
         
         for i in range(1, 4):
             self.assertEqual([r.mutual for r in friendship.friends(user_ids[i]).select()], [1, 1])
         
-        friendship.request_friend(user_ids[1], user_ids[4])
-        friendship.accept_friend(user_ids[4], user_ids[1])
+        friendship.add_friend(user_ids[1], user_ids[4])
+        friendship.confirm_friend(user_ids[4], user_ids[1])
         
-        friendship.request_friend(user_ids[2], user_ids[4])
-        friendship.accept_friend(user_ids[4], user_ids[2])
+        friendship.add_friend(user_ids[2], user_ids[4])
+        friendship.confirm_friend(user_ids[4], user_ids[2])
         
         self.assertEqual([r.mutual for r in friendship.friends(user_ids[1]).select()], [2, 1, 1])
         self.assertEqual([r.mutual for r in friendship.friends(user_ids[2]).select()], [2, 1, 1])
@@ -158,21 +165,21 @@ class TestFriendship(unittest.TestCase):
         for i in range(1, 4):
             self.assertEqual([r.mutual for r in friendship.friends(user_ids[i]).select()], [1, 1])
         
-    def test_deny_friend(self):
-        friendship.request_friend(user_ids[1], user_ids[2])
-        friendship.deny_friend(user_ids[2], user_ids[1])
+    def test_ignore_friend(self):
+        friendship.add_friend(user_ids[1], user_ids[2])
+        friendship.ignore_friend(user_ids[2], user_ids[1])
         self.assertEqual(friendship.friend_requets(user_ids[1]).count(), 0)
         self.assertEqual(friendship.friend_requets(user_ids[2]).count(), 0)
         self.assertEqual(friendship.friends(user_ids[1]).count(), 0)
         self.assertEqual(friendship.friends(user_ids[2]).count(), 0)
-        self.assertRaises(ValueError, friendship.request_friend, user_ids[1], user_ids[2])
         
     def test_extra_fields(self):
-        friendship.request_friend(user_ids[1], user_ids[2])
-        friendship.accept_friend(user_ids[2], user_ids[1])
+        friendship.add_friend(user_ids[1], user_ids[2])
+        friendship.confirm_friend(user_ids[2], user_ids[1])
         friend = friendship.friend(user_ids[1], user_ids[2]).select().first()
         self.assertEqual(friend.affinity, 1.0)
         
 def test():
-    return dict(output=CODE(run_test(TestFriendship)))
+    return dict(back=A('back', _href=URL('index')),
+                output=CODE(run_test(TestFriendship)))
     
--- modules/plugin_friendship.py
+++ modules/plugin_friendship.py
@@ -13,14 +13,13 @@ class Friendship(object):
         
         settings.extra_fields = {}
         
-        settings.onaccept = None
+        settings.onconfirm = None
         
         settings.table_friend_name = 'friend'
         settings.table_friend = None
         
         settings.status_requesting = 'r'
-        settings.status_accepted = 'a'
-        settings.status_denied = 'd'
+        settings.status_confirmed = 'c'
         
     def define_tables(self, table_user_name, migrate=True, fake_migrate=False):
         db, settings = self.db, self.settings
@@ -31,12 +30,12 @@ class Friendship(object):
                 Field('user', 'reference %s' % table_user_name),
                 Field('friend', 'reference %s' % table_user_name),
                 Field('status', length=1, default=settings.status_requesting,
-                      requires=[settings.status_requesting, settings.status_accepted, settings.status_denied]),
+                      requires=[settings.status_requesting, settings.status_confirmed]),
                 Field('mutual', 'integer', default=0),
                 migrate=migrate, fake_migrate=fake_migrate,
                 *settings.extra_fields.get(settings.table_friend_name, []))
  
-    def request_friend(self, user_id, friend_id):
+    def add_friend(self, user_id, friend_id):
         db, settings, table = self.db, self.settings, self.settings.table_friend
         if user_id == friend_id:
             raise ValueError
@@ -51,7 +50,7 @@ class Friendship(object):
         
         return db(table.friend==user_id)(table.status==settings.status_requesting)
     
-    def accept_friend(self, user_id, friend_id):
+    def confirm_friend(self, user_id, friend_id):
         db, settings, table = self.db, self.settings, self.settings.table_friend
         
         if not db(table.friend==user_id)(table.user==friend_id)(
@@ -61,12 +60,12 @@ class Friendship(object):
         mutual_friends = (set([r.friend for r in self.friends(user_id).select(table.friend)]) &
                           set([r.friend for r in self.friends(friend_id).select(table.friend)]))
         for _user_id in (user_id, friend_id):
-            db(table.user==_user_id)(table.status==settings.status_accepted)(
+            db(table.user==_user_id)(table.status==settings.status_confirmed)(
                table.friend.belongs(mutual_friends)).update(mutual=table.mutual+1)
-            db(table.friend==_user_id)(table.status==settings.status_accepted)(
+            db(table.friend==_user_id)(table.status==settings.status_confirmed)(
                table.user.belongs(mutual_friends)).update(mutual=table.mutual+1)
             
-        updator = dict(status=settings.status_accepted, 
+        updator = dict(status=settings.status_confirmed, 
                        mutual=len(mutual_friends))
             
         db(table.friend==user_id)(table.user==friend_id).update(**updator)
@@ -76,42 +75,52 @@ class Friendship(object):
         else:
             table.insert(user=user_id, friend=friend_id, **updator)
            
-        if settings.onaccept:
-            settings.onaccept(user_id, friend_id)
+        if settings.onconfirm:
+            settings.onconfirm(user_id, friend_id)
         
     def friends(self, user_id):
         db, settings, table = self.db, self.settings, self.settings.table_friend
-        return db(table.user==user_id)(table.status==settings.status_accepted)
+        return db(table.user==user_id)(table.status==settings.status_confirmed)
         
     def friend(self, user_id, friend_id):
         db, settings, table = self.db, self.settings, self.settings.table_friend
         return self.friends(user_id)(table.friend==friend_id)
     
-    def deny_friend(self, user_id, friend_id):
+    def ignore_friend(self, user_id, friend_id):
         db, settings, table = self.db, self.settings, self.settings.table_friend
         
         if not db(table.friend==user_id)(table.user==friend_id)(
                   table.status==settings.status_requesting).count():
             raise ValueError
             
-        db(table.friend==user_id)(table.user==friend_id).update(
-            status=settings.status_denied)
+        db(table.friend==user_id)(table.user==friend_id).delete()
             
     def remove_friend(self, user_id, friend_id):
         db, settings, table = self.db, self.settings, self.settings.table_friend
         
         if not db(table.user==user_id)(table.friend==friend_id)(
-                  table.status==settings.status_accepted).count():
+                  table.status==settings.status_confirmed).count():
             raise ValueError
             
         mutual_friends = (set([r.friend for r in self.friends(user_id).select(table.friend)]) &
                           set([r.friend for r in self.friends(friend_id).select(table.friend)]))
         for _user_id in (user_id, friend_id):
-            db(table.user==_user_id)(table.status==settings.status_accepted)(
+            db(table.user==_user_id)(table.status==settings.status_confirmed)(
                table.friend.belongs(mutual_friends)).update(mutual=table.mutual-1)
-            db(table.friend==_user_id)(table.status==settings.status_accepted)(
+            db(table.friend==_user_id)(table.status==settings.status_confirmed)(
                table.user.belongs(mutual_friends)).update(mutual=table.mutual-1)
                
         db(table.user==user_id)(table.friend==friend_id).delete()
         db(table.friend==user_id)(table.user==friend_id).delete()
+        
+    def refresh_all_mutuals(self):
+        # ! Be careful when using it. It will require much time.
+        db, settings, table = self.db, self.settings, self.settings.table_friend
+        records = db(table.id>0).select()
+        for record in records:
+            user_id = record.user
+            friend_id = record.friend
+            mutual_friends = (set([r.friend for r in self.friends(user_id).select(table.friend)]) &
+                          set([r.friend for r in self.friends(friend_id).select(table.friend)]))
+            db(table.user==user_id)(table.friend==friend_id).update(mutual=len(mutual_friends))
         
