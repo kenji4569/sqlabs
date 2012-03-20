2fd41d624b3a1c93330735ebef482736c48964a9
Hosoda Kenji <hosoda@s-cubism.jp>
2012-03-20 14:25:49 +0900

    fix for inter-application sharing clean code on pep rules

--- modules/plugin_uploadify_widget.py
+++ modules/plugin_uploadify_widget.py
@@ -7,12 +7,19 @@ from gluon.sqlhtml import UploadWidget
 from gluon.storage import Storage
 from gluon.contrib import simplejson as json
 
-FILES = ( URL('static', 'plugin_uploadify_widget/uploadify.css'),
-          URL('static', 'plugin_uploadify_widget/swfobject.js'),
-          URL('static', 'plugin_uploadify_widget/jquery.uploadify.v2.1.4.min.js'),
-          URL('static', 'plugin_uploadify_widget/uploadify.css'),
-         )
+# For referencing static and views from other application
+import os
+APP = os.path.basename(os.path.dirname(os.path.dirname(__file__)))
+
+FILES = (
+    URL(APP, 'static', 'plugin_uploadify_widget/uploadify.css'),
+    URL(APP, 'static', 'plugin_uploadify_widget/swfobject.js'),
+    URL(APP, 'static', 'plugin_uploadify_widget/jquery.uploadify.v2.1.4.min.js'),
+    URL(APP, 'static', 'plugin_uploadify_widget/uploadify.css'),
+)
+
 BUTTON_TEXT = 'SELECT FILES'
+
 
 def _set_files(files):
     if current.request.ajax:
@@ -25,20 +32,25 @@ $.each(%s, function() {
         document.body.appendChild(el);
     } else if ((this.slice(-4) == '.css') && ($.inArray(this.toString(), hrefs) == -1)) {
         $('<link rel="stylesheet" type="text/css" href="' + this + '" />').prependTo('head');
-        if (/* for IE */ document.createStyleSheet){document.createStyleSheet(this);} 
+        if (/* for IE */ document.createStyleSheet){document.createStyleSheet(this);}
 }});})(jQuery);""" % ('[%s]' % ','.join(["'%s'" % f.lower().split('?')[0] for f in files]))
     else:
         current.response.files[:0] = [f for f in files if f not in current.response.files]
-    
+
+
 class IS_UPLOADIFY_IMAGE(IS_IMAGE):
-    def _call(self, value): 
+
+    def _call(self, value):
         return IS_IMAGE.__call__(self, value)
-    def __call__(self, value): 
+
+    def __call__(self, value):
         if not value:
             return (value, translate(self.error_message))
         return (value, None)
-        
+
+
 class IS_UPLOADIFY_FILENAME(IS_UPLOAD_FILENAME):
+
     def __init__(self, filename=None, extension=None, lastdot=True, case=1,
                     error_message='enter valid filename'):
         import re
@@ -48,36 +60,40 @@ class IS_UPLOADIFY_FILENAME(IS_UPLOAD_FILENAME):
             self._extension = None
         IS_UPLOAD_FILENAME.__init__(self, filename, extension, lastdot, case, error_message)
         
-    def _call(self, value): 
+    def _call(self, value):
         return IS_UPLOAD_FILENAME.__call__(self, value)
-    def __call__(self, value): 
+
+    def __call__(self, value):
         if not value:
             return (value, translate(self.error_message))
         return (value, None)
-        
+
+
 class IS_UPLOADIFY_LENGTH(IS_LENGTH):
     def __init__(self, maxsize=255, minsize=0,
                  error_message='enter a file from %(min)g to %(max)g KB'):
         IS_LENGTH.__init__(self, maxsize, minsize, error_message)
         
-    def _call(self, value): 
+    def _call(self, value):
         return IS_LENGTH.__call__(self, value)
-    def __call__(self, value): 
+
+    def __call__(self, value):
         if not value:
             return (value, translate(self.error_message) % dict(
                                 min=self.minsize / 1024, max=self.maxsize / 1024))
         return (value, None)
 
-def uploadify_widget(field, value, download_url=None, **attributes): 
+
+def uploadify_widget(field, value, download_url=None, **attributes):
     _set_files(FILES)
     
     _id = '%s_%s' % (field._tablename, field.name)
-    input_el = INPUT(_id = _id, _name = field.name,
-                     requires = field.requires, _type='hidden',)
+    input_el = INPUT(_id=_id, _name=field.name,
+                     requires=field.requires, _type='hidden',)
 
     from gluon.utils import web2py_uuid
     _file_id = '__uploadify__%s_%s_%s' % (field._tablename, field.name, web2py_uuid())
-    file_input_el = INPUT(_id = _file_id, _name = '__uploadify__%s' % field.name, _type='file')
+    file_input_el = INPUT(_id=_file_id, _name='__uploadify__%s' % field.name, _type='file')
 
     ajaxed = 'Filedata' in current.request.post_vars and current.request.post_vars.name == field.name
         
@@ -91,7 +107,7 @@ def uploadify_widget(field, value, download_url=None, **attributes):
     for i, r in enumerate(requires):
         
         if isinstance(r, IS_UPLOADIFY_IMAGE):
-            fileext = ';'.join(['*.%s' % ext if ext!='jpeg' else '*.jpeg;*.jpg' for ext in r.extensions])
+            fileext = ';'.join(['*.%s' % ext if ext != 'jpeg' else '*.jpeg;*.jpg' for ext in r.extensions])
         elif isinstance(r, IS_UPLOADIFY_FILENAME):
             if r._extension:
                 fileext = '*.%s' % r._extension
@@ -131,7 +147,7 @@ form_el.bindFirst('submit', function (e) {
             return !%(ajax)s;
         } else {
             if (%(ajax)s) { e.stopImmediatePropagation(); }
-            file_el.uploadifyUpload(); 
+            file_el.uploadifyUpload();
             return false;
         }
     }
@@ -174,12 +190,12 @@ file_el.uploadify({
     'scriptData': scriptData
 });
 })()) {setTimeout(run, t); t = 2*t;}})();});
-""" % dict(id=_id, file_id=_file_id, 
+""" % dict(id=_id, file_id=_file_id,
            button_text=BUTTON_TEXT,
-           uploader=URL('static', 'plugin_uploadify_widget/uploadify.swf'),
+           uploader=URL(APP, 'static', 'plugin_uploadify_widget/uploadify.swf'),
            script=URL(args=current.request.args),
            extra_vars=json.dumps(attributes.get('extra_vars', {})),
-           cancel_img=URL('static', 'plugin_uploadify_widget/cancel.png'),
+           cancel_img=URL(APP, 'static', 'plugin_uploadify_widget/cancel.png'),
            fileext=fileext,
            size_limit=size_limit,
            ajax='true' if current.request.ajax else 'false',
@@ -188,7 +204,7 @@ file_el.uploadify({
     if '_formkey' in current.request.vars:
         filename = current.request.vars[field.name]
         if filename:
-            current.request.vars[field.name] = Storage(file=None, filename=filename) # TODO file
+            current.request.vars[field.name] = Storage(file=None, filename=filename)  # TODO file
             field.store = lambda source_file, original_filename, path=None: original_filename
         
     inp = SPAN(input_el, file_input_el)
@@ -202,12 +218,12 @@ file_el.uploadify({
         (br, image) = ('', '')
         if UploadWidget.is_image(value):
             br = BR()
-            image = IMG(_src = url, _width = UploadWidget.DEFAULT_WIDTH)
+            image = IMG(_src=url, _width=UploadWidget.DEFAULT_WIDTH)
 
         requires = field.requires
         if requires == [] or isinstance(requires, IS_EMPTY_OR):
             inp = DIV(inp, '[',
-                      A(UploadWidget.GENERIC_DESCRIPTION, _href = url),
+                      A(UploadWidget.GENERIC_DESCRIPTION, _href=url),
                       '|',
                       INPUT(_type='checkbox',
                             _name=field.name + UploadWidget.ID_DELETE_SUFFIX),
@@ -215,7 +231,7 @@ file_el.uploadify({
                       ']', br, image)
         else:
             inp = DIV(inp, '[',
-                      A(UploadWidget.GENERIC_DESCRIPTION, _href = url),
+                      A(UploadWidget.GENERIC_DESCRIPTION, _href=url),
                       ']', br, image)
             
     return DIV(script, inp, **attributes)
